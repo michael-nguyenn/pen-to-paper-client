eab3f58c44ad3f4018d69b1ca610ef84354d5b75
Michael Nguyen <n93michael@gmail.com>
2022-07-02 18:17:06 -0400

    navigation styling done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/icons/logo.svg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="nav__container">
+          <Link to="" className="nav__link">
+            <img src={logo} alt="site logo" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
