1faf6d9a31629780a7c3c533926440ef10e904dd
Michael Nguyen <n93michael@gmail.com>
2022-07-04 14:41:25 -0400

    styling done for sign up and login forms

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,22 +3,57 @@ import "./Login.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import login from "../../assets/images/log-in.jpg";
+
+import { Link } from "react-router-dom";
+
 function Login() {
+  const handleLogin = () => {
+    window.location.replace("http://localhost:3000/user");
+  };
+
   return (
     <>
       <Header />
       <section className="login">
-        <div className="login__left">
-          <img src="" alt="login" className="login__image" />
-        </div>
+        <div className="login__container">
+          <div className="login__left">
+            <img src={login} alt="login" className="login__image" />
+          </div>
 
-        <div className="login__right">
-          <form>
-            <label>
-              Username
-              <input></input>
-            </label>
-          </form>
+          <div className="login__right">
+            <form className="login__form">
+              <h1 className="login__title">Login</h1>
+              <label className="login__label">
+                Username
+                <input type="text" name="username" className="login__input" />
+              </label>
+
+              <label className="login__label">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  className="login__input"
+                />
+              </label>
+
+              <div className="login__button">
+                <button
+                  className="button button--login"
+                  type="button"
+                  onClick={handleLogin}
+                >
+                  Login
+                </button>
+                <div className="login__wrapper">
+                  <Link to="/signup" className="login__link">
+                    Don't have an account yet?
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </section>
       <Footer />
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -3,10 +3,56 @@ import "./SignUp.scss";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import signup from "../../assets/images/sign-up.jpg";
+
 function SignUp() {
+  const handleSignup = () => {
+    window.location.replace("http://localhost:3000/login");
+  };
   return (
     <>
       <Header />
+      <section className="login">
+        <div className="login__container login__container--special">
+          <div className="login__left">
+            <img src={signup} alt="login" className="login__image" />
+          </div>
+
+          <div className="login__right">
+            <form className="login__form">
+              <h1 className="login__title">Sign up</h1>
+              <label className="login__label">
+                Name
+                <input type="text" name="username" className="login__input" />
+              </label>
+
+              <label className="login__label">
+                Username
+                <input type="text" name="username" className="login__input" />
+              </label>
+
+              <label className="login__label">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  className="login__input"
+                />
+              </label>
+
+              <div className="login__button">
+                <button
+                  onClick={handleSignup}
+                  className="button button--login"
+                  type="button"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
